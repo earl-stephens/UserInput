d760a8dfb5c0fd7cba787a9808fec04f09441467
earl-stephens <stephensearl@hotmail.com>
2021-12-15 23:28:31 -0700

    Added code to accept user input

--- src/application/App.java
+++ src/application/App.java
@@ -1,9 +1,24 @@
 package application;
 
+//keyboard shortcut to get this package is Cmd+Shift+o
+//Scanner is in the java.util package
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Scanner is the class; scanner is an instance of the class
+		//IOW, instantiating a new scanner object
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter value in Fahrenheit to convert > ");
+		
+		double fahrenheit = scanner.nextFloat();
+		
+		double celsius = (fahrenheit - 32) * 5.0 / 9.0;
+		
+		System.out.printf("%.2f degrees Fahrenheit is %.2f degrees celsius.\n", fahrenheit, celsius);
 
 	}
 
